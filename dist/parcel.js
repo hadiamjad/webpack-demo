e1d9f6874f181685093ed7d16b27c16a468659c2
Hadi Amjad <hadiyoville25@gmail.com>
2022-06-15 13:43:34 -0400

    basic testing

---
+++ dist/parcel.js
@@ -0,0 +1,20 @@
+var $gXNCa$lodash = require("lodash");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+function $4fa36e821943b400$var$component() {
+    const element = document.createElement("div");
+    // - Lodash, currently included via a script, is required for this line to work
+    // + Lodash, now imported by this script
+    element.innerHTML = (0, ($parcel$interopDefault($gXNCa$lodash))).join([
+        "Hello",
+        "webpack"
+    ], " ");
+    return element;
+}
+document.body.appendChild($4fa36e821943b400$var$component());
+
+
+//# sourceMappingURL=main2.js.map
